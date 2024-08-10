b61899118b78dd6b5fabf33023e16ba4a7d3d070
Jaakko Hirvioja <jhirvioja@gmail.com>
2024-08-10 10:38:17 +0300

    Exercise 1.07: External access with Ingress

--- log-output/main.go
+++ log-output/main.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+type Status struct {
+	Timestamp string
+	Rstring   string
+}
+
+var status Status
+var mu sync.RWMutex
+
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	fmt.Printf("Server started in port %s\n", port)
+
 	randomString := uuid.New().String()
 
-	for {
-		currentTime := time.Now().Format(time.RFC3339Nano)
-		fmt.Printf("%s: %s\n", currentTime, randomString)
-		time.Sleep(5 * time.Second)
+	go func() {
+		for {
+			currentTime := time.Now().Format(time.RFC3339Nano)
+			mu.Lock()
+			status = Status{Timestamp: currentTime, Rstring: randomString}
+			mu.Unlock()
+			fmt.Printf("%s: %s\n", currentTime, randomString)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	http.HandleFunc("/", statusHandler)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Server error:", err)
 	}
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	mu.RLock()
+	defer mu.RUnlock()
+
+	fmt.Fprintf(w, "Timestamp: %s, String: %s\n", status.Timestamp, status.Rstring)
 }
