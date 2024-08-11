0dfcf8f7975f92b53f3f841e0d5f846bcd1ac2f7
Jaakko Hirvioja <jhirvioja@gmail.com>
2024-08-11 11:20:35 +0300

    Exercise 1.11: Persisting data

--- log-output/log-outputter/main.go
+++ log-output/log-outputter/main.go
@@ -17,18 +17,26 @@ func main() {
 	fmt.Printf("Server started on port %s\n", port)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		filePath := "/usr/src/app/files/timestamp.txt"
+		timestampFilePath := "/usr/src/app/files/timestamp.txt"
+		counterFilePath := "/usr/src/app/files/pongcounter.txt"
 
-		timestamp, err := os.ReadFile(filePath)
+		timestamp, err := os.ReadFile(timestampFilePath)
 		if err != nil {
-			fmt.Printf("Failed to read the file: %v\n", err)
+			fmt.Printf("Failed to read the timestamp file: %v\n", err)
 			http.Error(w, "Failed to read the timestamp file", http.StatusInternalServerError)
+			return
+		}
+
+		counter, err := os.ReadFile(counterFilePath)
+		if err != nil {
+			fmt.Printf("Failed to read the pong counter file: %v\n", err)
+			http.Error(w, "Failed to read the pong counter file", http.StatusInternalServerError)
 			return
 		}
 
 		randomString := uuid.New().String()
 
-		fmt.Fprintf(w, "Timestamp: %s, String: %s\n", timestamp, randomString)
+		fmt.Fprintf(w, "%s: %s.\nPing / Pongs: %s\n", string(timestamp), randomString, string(counter))
 	})
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
--- ping-pong/main.go
+++ ping-pong/main.go
@@ -9,11 +9,12 @@ import (
 
 var counter int
 var mu sync.Mutex
+var filePath = "/usr/src/app/files/pongcounter.txt"
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3001"
+		port = "3002"
 	}
 
 	fmt.Printf("Server started in port %s\n", port)
@@ -29,6 +30,28 @@ func pongHandler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	fmt.Fprintf(w, "pong %d", counter)
+	err := writeCounterToFile(counter)
+	if err != nil {
+		http.Error(w, "Unable to write to file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%d", counter)
+
 	counter++
+}
+
+func writeCounterToFile(counter int) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "%d\n", counter)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
