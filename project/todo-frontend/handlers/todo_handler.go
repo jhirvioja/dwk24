2080b54f8d9453f89478f59d5125f23fc574d4c4
Jaakko Hirvioja <jhirvioja@gmail.com>
2024-08-30 09:14:33 +0300

    Update HTML to be inside tmpl file, parse in handler

--- project/todo-frontend/handlers/todo_handler.go
+++ project/todo-frontend/handlers/todo_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/jhirvioja/dwk24/project/todo-frontend/services"
 )
@@ -25,83 +26,12 @@ func TodoHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tmpl := `
-		<!DOCTYPE html>
-		<html lang="en">
-			<head>
-				<meta charset="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>DwK Project</title>
-				<script
-					defer
-					src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
-				></script>
-				<script src="https://cdn.tailwindcss.com"></script>
-			</head>
-			<body>
-				<div class="flex flex-col items-center">
-					<div class="flex flex-col items-center p-8">
-						<h1 class="text-3xl font-bold underline">DwK Project</h1>
-						<div class="m-4 border-2 rounded-lg">
-							<img
-								class="p-8 max-w-full"
-								src="/files/picsum_image.jpg"
-								alt="Lorem Picsum Random Image"
-							/>
-						</div>
-						<div
-							x-data="{
-								todos: {{.Todos}},
-								newTodo: '',
-								addTodo() {
-									if (this.newTodo.trim() !== '') {
-										fetch('/add_todo', {
-											method: 'POST',
-											headers: {
-												'Content-Type': 'application/json',
-											},
-											body: JSON.stringify({ todo: this.newTodo.trim() }),
-										})
-										.then(response => response.json())
-										.then(data => {
-											this.todos = data;
-											this.newTodo = '';
-										})
-										.catch(error => console.error('Error:', error));
-									}
-								}
-							}"
-							class="flex flex-col p-4 border-2 rounded-lg w-80"
-						>
-							<h2 class="mb-4 text-2xl font-bold">Todos</h2>
-							<div class="m-2 flex flex-col">
-								<label for="todoinput">Syötä todo:</label>
-								<input
-									id="todoinput"
-									class="mt-2 rounded-md border-2"
-									maxlength="140"
-									x-model="newTodo"
-									@keydown.enter="addTodo"
-								/>
-								<button class="rounded-md border-2 mt-2 max-w-40" @click="addTodo">
-									Lisää todo
-								</button>
-							</div>
-							<ul class="mt-2 pl-8 list-disc">
-								<template x-for="todo in todos" :key="todo.id">
-									<li x-text="todo.todo"></li>
-								</template>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</body>
-		</html>
-	`
+	todoTemplatePath := filepath.Join("templates", "todo.tmpl")
 
-	t, err := template.New("webpage").Parse(tmpl)
+	t, err := template.ParseFiles(todoTemplatePath)
 	if err != nil {
-		http.Error(w, "Failed to parse template", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse template file: %v\n", err)
+		http.Error(w, "Failed to parse template file", http.StatusInternalServerError)
 		return
 	}
 
