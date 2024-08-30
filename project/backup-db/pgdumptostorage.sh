c4b9cc1885fcecb3050df8162572ca24bd348bb4
Jaakko Hirvioja <jhirvioja@gmail.com>
2024-08-30 21:05:15 +0300

    Exercise 3.07: Backup

---
+++ project/backup-db/pgdumptostorage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+DB_USERNAME=${DB_USERNAME}
+DB_PASSWORD=${DB_PASSWORD}
+DB_DATABASE=${DB_DATABASE}
+GCS_BUCKET=${GCS_BUCKET}
+
+DUMP_FILE="${DB_DATABASE}-$(date +%Y%m%d%H%M%S).sql"
+
+echo "Creating PostgreSQL dump.."
+PGPASSWORD=$DB_PASSWORD pg_dump -U $DB_USERNAME -d $DB_DATABASE -F c -b -v -f $DUMP_FILE
+
+echo "Uploading dump to Google Cloud Storage.."
+gsutil cp $DUMP_FILE gs://$GCS_BUCKET/$DUMP_FILE
+
+echo "Cleaning up.."
+rm $DUMP_FILE
+
+echo "Backup completed and uploaded to $GCS_BUCKET successfully."
